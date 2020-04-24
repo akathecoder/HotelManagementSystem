21d47601abe5770277a97812c69419e8ce7506e2
Priya Kaushik <priyakaushik2001@gmail.com>
2020-04-24 23:10:21 +0530

    integrate project for the first time

--- src/hotelmanagementsystem/DBConnect.java
+++ src/hotelmanagementsystem/DBConnect.java
@@ -25,7 +25,7 @@ public class DBConnect {
     public static void main(String[] args) {
         
         new DBConnect().DBCon();
-        new userLoginForm().setVisible(true);
+        new Bookingpage().setVisible(true);
         
     }
     
